5404a60cd15919e4da1234634378f3e44350b3b4
mbaguszulmi <baguszulmi101198@gmail.com>
2018-07-01 19:47:08 +0700

    First Test commit file to github with VSCode!

---
+++ hello.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Hello World");
+        System.out.printf("Please input your full name : ");
+        String name = userIn.nextLine();
+        System.out.printf("\nYour name is %s\n", name);
+        userIn.close();
+    }
+}
